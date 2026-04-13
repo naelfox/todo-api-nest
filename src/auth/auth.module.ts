ff99ede79c9f3e7c8e1c5e0d60e5a962cb979d57
Natanael Vilaça <63889443+naelfox@users.noreply.github.com>
2026-04-13 17:41:11 -0300

    feat: adiciona módulo Users ao AuthModule e exporta serviço

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,11 +3,13 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
+import { UsersModule } from '../users/users.module';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
   imports: [
+    UsersModule,
     JwtModule.register({
       secret: process.env.JWT_SECRET || 'supersecretkey',
       signOptions: { expiresIn: '1h' },
